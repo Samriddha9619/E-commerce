c32d2eeec1b52650d28ceef210539f1ff3a98bb7
Samriddha9619 <sumitkumartripathi0@gmail.com>
2025-02-25 01:17:36 +0530

    commit

--- MAC/shop/views.py
+++ MAC/shop/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Index shop")
+    return render(request,"shop/index.html")
+def about(request):
+    return HttpResponse("About shop")
+def contact(request):
+    return HttpResponse("We are at Contact")
+def tracker(request):
+    return HttpResponse("We are at Tracker")
+def search(request):
+    return HttpResponse("We are at Search")
+def productView(request):
+    return HttpResponse("We are at ProductView")
+def checkout(request):
+    return HttpResponse("We are at checkout")
